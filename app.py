9b703aa24b4a247d5c00aa03b18385101cc192e6
Ovsenka <ovsenka3@gmail.com>
2022-02-14 20:26:40 +0300

    base page

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from distutils.log import debug
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "12334823u5hdnkjxbnmvbi23u23u4235h235k"
 
-print(__name__)
 
 @app.route("/")
-def on_main():
-    return render_template("practice.html", title="Главная страница")
+def main():
+    return render_template("practice.html", title="Авторизация")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", title="Contact")
+
+@app.errorhandler(404)
+def notfound():
+    return render_template("error404.html", title="Страница не найдена")
 
 if __name__ == "__main__":
     app.run()
