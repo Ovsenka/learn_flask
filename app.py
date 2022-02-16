62bb505e462dc1b75e8ba58e665e9a3961f51199
Ovsenka <ovsenka3@gmail.com>
2022-02-16 18:55:11 +0300

    login page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from distutils.log import debug
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, session
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "12334823u5hdnkjxbnmvbi23u23u4235h235k"
@@ -7,15 +7,28 @@ app.config["SECRET_KEY"] = "12334823u5hdnkjxbnmvbi23u23u4235h235k"
 
 @app.route("/")
 def main():
-    return render_template("practice.html", title="Авторизация")
+    return render_template("practice.html", title="Hello")
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html", title="Contact")
 
+@app.route("/register")
+def register():
+    return render_template("register.html", title="Регистрация")
+
+@app.route("/login")
+def login():
+    return render_template("login.html", title="Войти")
+
+'''
 @app.errorhandler(404)
 def notfound():
     return render_template("error404.html", title="Страница не найдена")
+
+@app.errorhandler(500)
+def er():
+    return "LOL"'''
 
 if __name__ == "__main__":
     app.run()
