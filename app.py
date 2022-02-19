11fdae5a154968e15cd5dbdf114dc7c1fe7e6058
Ovsenka <ovsenka3@gmail.com>
2022-02-19 21:35:47 +0300

    Add in profile view

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, flash, session, request, url_for, redi
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "12334823u5hdnkjxbnm@#$!@!vbi2/ggr/24@#$@#$3u23u4235h235k"
 
-users = []
+users = {}
 
 @app.route("/")
 def main():
@@ -23,10 +23,8 @@ def contact():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST" and request.form["psw"] == request.form["psw-repeat"]:
-        print(request.form["optionsRadios"])
         session['user_logged'] = request.form['username']
-        print(session)
-        users.append((request.form['username'], request.form["psw"]))
+        users[request.form['username']] = ( request.form["psw"], request.form["optionsRadios"] )
         return redirect(url_for("profile", username=session["user_logged"]))
     elif "user_logged" in session:
         return redirect(url_for("profile", username=session["user_logged"]))
@@ -47,7 +45,7 @@ def login():
 def profile(username):
     if "user_logged" not in session or session["user_logged"] != username:
         abort(401)
-    return render_template("profile.html", user=username, title=username, is_logged=True)
+    return render_template("profile.html", user=username, sex=users[session["user_logged"]][1], title=username, is_logged=True)
 
 @app.route("/logout", methods=["POST", "GET"])
 def logout():
